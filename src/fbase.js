57c0b136738dc407be51f9bd250af28f26992485
hyejin <hyejin@hyejins-macbook.local>
2023-05-07 17:31:41 +0900

    0531

---
+++ src/fbase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCd6bZWVMldbGNF8AVJ_uqnDkUQfKNlr2o",
+    authDomain: "cafein-dd39e.firebaseapp.com",
+    projectId: "cafein-dd39e",
+    storageBucket: "cafein-dd39e.appspot.com",
+    messagingSenderId: "298159218568",
+    appId: "1:298159218568:web:ba8d244cfd92458749e43d"
+};
+
+export default firebase.initializeApp(firebaseConfig);
