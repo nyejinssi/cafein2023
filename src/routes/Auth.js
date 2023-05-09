4c956d022506ccd69ddf4f61d8de36f61d5d460b
hyejin <hyejin@hyejins-macbook.local>
2023-05-09 16:39:16 +0900

    2.3 done

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { authService } from '../fbase';
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true); 
 
     const onChange = (event) => {
         const {target: {name, value}} = event;
@@ -15,6 +17,17 @@ const Auth = () => {
 
     const onSubmit = (event) => {
         event.preventDefault();
+        try {
+            let data;
+            if(newAccount){
+                data = authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                data = authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        } catch(error) {
+            console.log(error);
+        }
     };
 
     return (
@@ -22,10 +35,10 @@ const Auth = () => {
             <form onSubmit={onSubmit}>
                 <input name="email" type="text" placeholder="Email" required value = {email} onChange={onChange}/>
                 <input name="password" type="password" placeholder="Password" required value = {password} onChange={onChange} />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
             </form>
             <div>
-                <button>Continue with Google</button>
+                <button> 구글로 로그인 </button>
             </div>
         </div>
     )
