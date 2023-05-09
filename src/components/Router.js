4d601133efbf5097ed9188873ccf7acc3d9724b6
hyejin <hyejin@hyejins-macbook.local>
2023-05-09 18:08:03 +0900

    log in & log out Done

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,21 +1,26 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from 'react';
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "./Navigation";
+import Profile from "../routes/Profile";
 
 const AppRouter = ({isLoggedIn}) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation/>}
             <Routes>
                 {isLoggedIn ? (
                 <>
                     <Route exact path="/" element ={<Home/>}/>
+                    <Route exact path="/profile" element ={<Profile/>}/>
                 </> 
                 ): (
                     <Route exact path="/" element ={<Auth/>}/>
                 )}
             </Routes>
-        </Router>)
+        </Router>
+        )
 }
 
 export default AppRouter;
